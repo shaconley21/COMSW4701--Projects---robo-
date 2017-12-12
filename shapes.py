295b959218d5b6a0ff08fa0a96f92b01603e6aaf
Sarina Xie <sx2166@columbia.edu>
2017-12-12 13:48:58 -0500

    woooo rrt finds the goal

--- shapes.py
+++ shapes.py
@@ -21,130 +21,138 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
 class Node:
-    x = 0
-    y = 0  
-    parent = None
-    cost = 0 
-    def __init__(self, xcoord, ycoord):
-        self.x = xcoord
-        self.y = ycoord
-
-def initialize():
-    pygame.init()
-    screen = pygame.display.set_mode(WINDOW)
-    pygame.display.set_caption('HOMEWORK 5')
-
-    POLY_LIST = obstacle_crtr("world_obstacles.txt", screen)
+	x = 0
+	y = 0  
+	parent = None
+	cost = 0 
+	def __init__(self, xcoord, ycoord):
+		self.x = xcoord
+		self.y = ycoord
 
 # add obstacles to the screen | return list of obstacles
 def obstacle_crtr(filename, screen):
-    with open(filename, "r") as obstacles:
-        for line in obstacles:
-            x = line.split(" ")
-            if len(x) == 1:
-                n_corners = int(x[0])
-                points = []
-                cntr = 0
-            else:
-                points.append((int(x[0]), int(x[1])))
-                POLY_LIST.append(points)
-                cntr += 1
-                if cntr == n_corners:
-                    polygon(screen, YELLOW, points, 1)
+	with open(filename, "r") as obstacles:
+		for line in obstacles:
+			x = line.split(" ")
+			if len(x) == 1:
+				n_corners = int(x[0])
+				points = []
+				cntr = 0
+			else:
+				points.append((int(x[0]), int(x[1])))
+				POLY_LIST.append(points)
+				cntr += 1
+				if cntr == n_corners:
+					polygon(screen, YELLOW, points, 1)
 
 # add start, goal to screen | return start, goal values
 def start_goal(filename, screen):
-    with open(filename, "r") as sg:
-        s = str.split(sg.readline())
-        start = (int(s[0]), int(s[1]))
-        circle(screen, GREEN, start, 3, 0)
+	with open(filename, "r") as sg:
+		s = str.split(sg.readline())
+		start = (int(s[0]), int(s[1]))
+		circle(screen, GREEN, start, 3, 0)
 
-        g = str.split(sg.readline())
-        goal = (int(g[0]), int(g[1]))
-        circle(screen, RED, goal, 3, 0)
+		g = str.split(sg.readline())
+		goal = (int(g[0]), int(g[1]))
+		circle(screen, RED, goal, 3, 0)
 
-        return start, goal
+		return start, goal
 
 # distance formula takes in 2 Nodes
 def dist(pt1,pt2):
-    return sqrt((pt1.x-pt2.x)*(pt1.x-pt2.x)+(pt1.y-pt2.y)*(pt1.y-pt2.y))
+	return sqrt((pt1.x-pt2.x)*(pt1.x-pt2.x)+(pt1.y-pt2.y)*(pt1.y-pt2.y))
 
 def get_random_pt():
-    while True:
-        a = int(random.random()*XDIM)
-        b = int(random.random()*YDIM)
-        p = Point(a,b)
-        if collide_pt(p) == False:
-            print "got random point"
-            return [a,b]   
+	while True:
+		a = int(random.random()*XDIM)
+		b = int(random.random()*YDIM)
+		p = Point(a,b)
+		if collide_pt(p) == False:
+			return [a,b]   
 
 def collide_pt(p):
 	for poly in POLY_LIST:
-        polygoly = Polygon([list(elem) for elem in poly])
-        if polygoly.intersects(obj):
-            return True
-    return False
+		polygoly = Polygon([list(elem) for elem in poly])
+		if polygoly.intersects(p):
+			return True
+	return False
 
 def collide_ln(ln):
 	for poly in POLY_LIST:
 		polygoly = Polygon([list(elem) for elem in poly])
-		for i in range(0, ln.length, 15):
-        	ip = ln.interpolate(i)
-        	if polygoly.intersects(ip):
-        		print True
-        		return True
-        		circle(screen, GREEN, [int(ip.x),int(ip.y)], 3, 0)
-        	else:
-        		print False
-        		return False
+		for i in range(0, int(ln.length), 5):
+			ip = ln.interpolate(i)
+			if polygoly.intersects(ip):
+				return True
+				circle(screen, GREEN, [int(ip.x),int(ip.y)], 3, 0)
+	return False
 
-#TODO
-def lines_cross(ln):
+def lines_cross(ln,min,max,screen):
+	for otherln in LINES:
+		if ln.crosses(otherln):
+			return True
+	return False
 
 
-def find_near_neighbor(pt):
-	print "finding near neighbor"
+def find_near_neighbor(pt, screen):
+	#print "finding near neighbor"
 	nearest = NODES[0]
+	ls = LineString([(0,0),(0,0)])
 	valid = False
 	for n in NODES:
-		#TODO
-		if dist(n,pt) < dist(nearest,pt) and dist(n,pt) > 20 and dist(n,pt) < 50:
-			ls = LineString([(n.x, n.y),(pt.x, pt.y)])
-			if collides_ln(ls) == False and lines_cross(ls) == False:
-				nearest = n
-				valid = True
-	return nearest, ls, valid
+		#print "dist:", dist(n,pt)
+		if dist(n,pt) < dist(nearest,pt):
+			 nearest = n
+	return nearest
+
+def step_to(pt, node, screen):
+	longls = LineString([(node.x, node.y),(pt.x, pt.y)])
+	ipt = longls.interpolate(15)
+	ls = LineString([(ipt.x, ipt.y),(node.x, node.y)])
+	if collide_ln(ls) == False and lines_cross(ls,ipt,node,screen) == False:
+		circle(screen, YELLOW, [int(ipt.x), int(ipt.y)], 3, 0)
+		print "collide_ln:", collide_ln(ls), "lines_cross:", lines_cross(ls,ipt,node,screen)
+		return ls, ipt, True
+	else:
+		return ls, ipt, False	
 
 def main():
 	#initialize screen with obstacles
-	initialize()
+	pygame.init()
+	screen = pygame.display.set_mode(WINDOW)
+	pygame.display.set_caption('HOMEWORK 5')
+	obstacle_crtr("world_obstacles.txt", screen)
 
 	start, goal = start_goal("start_goal.txt", screen)
-    NODES.append(Node(start[0], start[1]))
-    pygame.display.update()
+	NODES.append(Node(start[0], start[1]))
+	pygame.display.update()
 
-    done = False
-    while not done:
-    	valid = False
-    	while not valid:
-	    	listpt = get_random_pt()
-	    	pt = Point(listpt[0], listpt[1])
-	    	if collide_pt(pt) == False:
-	    		neighbor, ls, valid = find_near_neighbor(pt)
-	    node = Node(pt.x, pt.y)
-	    node.parent = neighbor
-	    NODES.append(node)
-	    LINES.append(ls)
-	    circle(screen, GREEN, listpt, 3, 0)
-        line(screen,WHITE,[neighbor.x,neighbor.y],listpt)
-        pygame.display.update()
+	done = False
+	while not done:
+		valid = False
+		while not valid:
+			listpt = get_random_pt()
+			pt = Point(listpt[0], listpt[1])
+			if collide_pt(pt) == False:
+				neighbor_node = find_near_neighbor(pt, screen)
+				ls, ipt, valid = step_to(pt, neighbor_node, screen)
+		node = Node(ipt.x, ipt.y)
+		node.parent = neighbor_node
+		NODES.append(node)
+		LINES.append(ls)
+		circle(screen, GREEN, [int(node.x),int(node.y)], 3, 0)
+		line(screen,WHITE,[neighbor_node.x,neighbor_node.y],[node.x,node.y])
+		pygame.display.update()
 
-        #checks if point is close enough to goal
-        if dist(point, goal) < 50:
-        	line(screen,RED,goal,listpt)
-            print "goal reached"
-            break
+		#checks if point is close enough to goal
+		if dist(pt, Point(goal[0], goal[1])) < 7:
+			#probably need to append node here
+			line(screen,RED,goal,listpt)
+			print "goal reached"
+			break
 
+	while not done:
+		pass
 
 if __name__ == '__main__':
-    main()
+	main()
